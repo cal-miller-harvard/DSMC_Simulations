dea56cce6e8bfa7006a582b389da9ce21a381fad
Cal <22692059+cal-miller-harvard@users.noreply.github.com>
2020-05-08 23:23:35 -0700

    improved run script

--- 5_6_20_three_stage_cell/run_cell.py
+++ 5_6_20_three_stage_cell/run_cell.py
@@ -9,12 +9,39 @@ import os
 flows = [4] # sccm
 gaps = [2E-3, 4E-3, 6E-3, 8E-3, 10E-3] # m
 lengths = [10E-3, 20E-3, 30E-3] # m
+omegas = [0]
+nparts = 500000
+
 for flow in flows:
     for gap in gaps:
         for length in lengths:
             directory = "flow_{:.3f}_gap_{:.3f}_len_{:.3f}".format(flow, gap, length)
             os.system("cp -rf template "+directory)
             os.chdir(directory)
+            for omega in omegas:
+                with open(r"particles_{}.slurm".format(omega), "w") as f:
+                    f.write("""#!/bin/bash
+#SBATCH -n 8 # Number of cores requested
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -t 2:00:00 # Runtime in minutes
+#SBATCH -p shared # Partition to submit to
+#SBATCH --mem-per-cpu 2048 # Memory per cpu in MB (see also ?mem-per-cpu)
+#SBATCH --open-mode=append
+#SBATCH -o particles_omega_{}_job_%j.out # Standard out goes to this file
+#SBATCH -e particles_omega_{}_job_%j.err # Standard err goes to this filehostname
+
+module load intel/19.0.5-fasrc01 openmpi/4.0.2-fasrc01 fftw/3.3.8-fasrc01 cmake/3.12.1-fasrc01 Anaconda3/2019.10 python/3.7.7-fasrc01
+module list
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export JULIA_NUM_THREADS=threads
+
+echo "running...."
+
+/n/home03/calmiller/programs/julia /n/home03/calmiller/DSMC_Simulations/ParticleTracing/ParticleTracing.jl -z 0.035 -T 2.0 -n {} ./cell.510001.surfs ./DS2FF.500000.DAT --omega {} --stats ./stats_omega_{}.csv --exitstats ./exitstats_omega_{}.csv
+
+""".format(omega, omega, nparts, omega, omega, omega))
             with open(r"in.cell", "w") as f:
                 f.write("""# 2d axial simulation of CBGB cell
 
@@ -104,5 +131,27 @@ jump in.cell loop2
 dump surfs surf all 1 data/cell.*.surfs id v1x v1y v2x v2y
 run 1
 """.format(gap, length, flow))
+            with open(r"in.cell", "w") as f:
+                f.write("""#!/bin/bash
+    #SBATCH -n 16 # Number of cores requested
+    #SBATCH -N 1 # Ensure that all cores are on one machine
+    #SBATCH -t 12:00:00 # Runtime in minutes
+    #SBATCH -p shared # Partition to submit to
+    #SBATCH --mem-per-cpu 1024 # Memory per cpu in MB (see also ?mem-per-cpu)
+    #SBATCH --open-mode=append
+    #SBATCH -o spa_%j.out # Standard out goes to this file
+    #SBATCH -e spa_%j.err # Standard err goes to this filehostname
+
+    module load intel/19.0.5-fasrc01 openmpi/4.0.2-fasrc01 fftw/3.3.8-fasrc01 cmake/3.12.1-fasrc01 Anaconda3/2019.10 python/3.7.7-fasrc01
+    module list
+
+    export OMP_PROC_BIND=spread
+    export OMP_PLACES=threads
+
+    echo "running...."
+
+    mpirun ~/programs/sparta/spa -kokkos off < in.cell
+
+    """.join(["sbatch particles_{}.slurm\n".format(omega) for omega in omegas]))
             os.system("sbatch run.slurm")
             os.chdir("..")
